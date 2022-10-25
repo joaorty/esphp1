ab250ee97e8253c61dc37d8a624953ef0ff01b70
joaorty <123321joao@gmail.com>
2022-10-25 09:23:56 -0300

    first commit

---
+++ includes/formulario.php
@@ -0,0 +1,33 @@
+<main>
+  <section>
+    <a href="cadastrar_tarefa.php">
+      <button class="btn btn-success">Nova Tarefa</button>
+    </a>
+  </section>
+
+  <h2 class="mt-3">Cadastrar Tarefa</h2>
+
+  <form action="" method="post">
+
+    <div class="form-group">
+      <label>Título</label>
+      <input type="text" class="form-control" name="titulo">
+    </div>
+
+    <div class="form-group">
+      <label>Título</label>
+      <input type="text" class="form-control" name="titulo">
+    </div>
+
+    <div class="form-group">
+      <label>Descrição</label>
+      <textarea class="form-control" name="descricao">
+    </div>
+    
+    <div class="form-group">
+      <label>Título</label>
+      <input type="text" class="form-control" name="titulo">
+    </div>
+
+  </form>
+</main>
---
+++ includes/listagem.php
@@ -0,0 +1,7 @@
+<main>
+  <section>
+    <a href="cadastrar_tarefa.php">
+      <button class="btn btn-success">Nova Tarefa</button>
+    </a>
+  </section>
+</main>
